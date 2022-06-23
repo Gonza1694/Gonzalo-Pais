3879ad130547a1238b4fe52101582825af29236b
Gonza <gonzaa1694@gmail.com>
2022-06-22 21:39:10 -0300

    add some navbar styles

--- 2 - Desafio Menú e-commerce/online-store/src/components/NavBar/NavBar.js
+++ 2 - Desafio Menú e-commerce/online-store/src/components/NavBar/NavBar.js
@@ -1,21 +1,29 @@
 import React from 'react'
-import { AppBar, Toolbar, IconButton } from '@mui/material'
+import { AppBar, Toolbar, IconButton, TextField } from '@mui/material'
 import { AccountCircle, ShoppingCart } from '@material-ui/icons';
 
+//TODO: Remove unusing divs/classes
 const NavBar = () => {
   return (
     <>
       <AppBar position="static">
         <Toolbar style={{ display: 'flex', justifyContent: 'space-between' }}>
-          <IconButton edge="start" color="inherit" aria-label="menu" style={{ marginRight: 'auto' }}>
-            Online Store
-          </IconButton>
-          <IconButton>
-            <ShoppingCart />
-          </IconButton>
-          <IconButton>
-            <AccountCircle/>
-          </IconButton>
+          <div className='logo'>
+            <IconButton edge="start" color="inherit" aria-label="menu" style={{ marginRight: 'auto' }}>
+              Online Store
+            </IconButton>
+          </div>
+          <div className='searchField'>
+            <TextField id="outlined-search" label="" type="search" variant="outlined" size='small' margin="dense" style={{backgroundColor:'#fff', borderRadius:'6px'}} placeholder="Search..." />
+          </div>
+          <div className='navButtons'>
+            <IconButton>
+              <ShoppingCart />
+            </IconButton>
+            <IconButton>
+              <AccountCircle />
+            </IconButton>
+          </div>
         </Toolbar>
       </AppBar>
     </>

7ec48aaf15448d8c12601c184bee4a8aec152146
Gonza <gonzaa1694@gmail.com>
2022-07-04 21:18:25 -0300

    change project name

--- 3 - Crea tu landing/baloo-home/src/components/ItemListContainer/ItemListContainer.js
+++ 3 - Crea tu landing/baloo-home/src/components/ItemListContainer/ItemListContainer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import myImg from '../../img/logo.png'
 const ItemListContainer = () => {
   return (
-    <img src={myImg}/>
+    <img src={myImg} alt="Baloo-Home logo"/>
   )
 }
 
--- 3 - Crea tu landing/baloo-home/src/components/NavBar/Logo/Logo.js
+++ 3 - Crea tu landing/baloo-home/src/components/NavBar/Logo/Logo.js
@@ -5,7 +5,7 @@ import myImg from '../../../img/logo-head.png'
 const Logo = () => {
   return (
     <div className='logo-container'>
-      <img src={myImg} className="logo" />
+      <img src={myImg} className="logo" alt="Baloo-Home-logo" />
       <h3>Baloo Home</h3>
     </div>
   )

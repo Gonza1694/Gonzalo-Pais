db7853e06fbedfc1067ac3333c2b750adaabb798
Gonza <gonzaa1694@gmail.com>
2022-07-06 20:43:14 -0300

    add async mock

--- 5 - Catálogo con MAPS y Promises/baloo-home/src/components/ItemListContainer/ItemList/ItemList.js
+++ 5 - Catálogo con MAPS y Promises/baloo-home/src/components/ItemListContainer/ItemList/ItemList.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import { CircularProgress } from '@mui/material';
+import React, { useEffect, useState } from 'react'
 import Item from '../Item/Item'
 
 const ItemList = ({ products }) => {
+
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(setLoading, 2000, false);
+    }, []);
+
     return (
         <>
-            {products.map((product) => (
-                <Item key={product.id} product={product} />
-            ))}
+            {loading
+                ? ( <CircularProgress disableShrink />)
+                : (products.map((product) => (
+                        <Item key={product.id} product={product} />
+                  )))
+            }
         </>
     )
 }

3a2930bcfa36f24e725f7218b2078bb382825be9
Gonza <gonzaa1694@gmail.com>
2022-07-18 15:24:52 -0300

    change category link

--- 7 - Primera Entrega del Proyecto Final/baloo-home/src/components/NavBar/Category/Category.js
+++ 7 - Primera Entrega del Proyecto Final/baloo-home/src/components/NavBar/Category/Category.js
@@ -7,16 +7,14 @@ import { Link, useParams } from 'react-router-dom'
 
 export default function Category() {
 
-    const { categoryId } = useParams()
-
     return (
         <FormControl sx={{ minWidth: 120, color: '#FFF' }} variant="standard" className="select-category">
             <Label id="demo-customized-select-label" sx={{ color: "#FFF" }}>Categoria</Label>
             <Select
                 defaultValue={''}>
-                <MenuItem value={'Cocina'} component={Link} to={`/category/${categoryId}`}>Cocina</MenuItem>
-                <MenuItem value={'Bano'} component={Link} to={`/category/${categoryId}`}>Baño</MenuItem>
-                <MenuItem value={'Habitacion'} component={Link} to={`/category/${categoryId}`}>Habitacion</MenuItem>
+                <MenuItem value={'Cocina'} component={Link} to={"/category/Cocina"}>Cocina</MenuItem>
+                <MenuItem value={'Bano'} component={Link} to={"/category/Bano"}>Baño</MenuItem>
+                <MenuItem value={'Habitacion'} component={Link} to={"/category/Habitacion"}>Habitacion</MenuItem>
             </Select>
         </FormControl>
     );
